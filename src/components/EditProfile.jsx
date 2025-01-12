84ba6ad3acc838a360d0c4c02920a6efbbd48e2a
SolareFlame <zeldac332@gmail.com>
2025-01-12 22:21:56 +0100

    🚀 New button to editProfile (back todo)

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,12 @@
+import {useTheme} from "../context/ThemeContext";
+
+import moon from '../assets/logo/moon.svg';
+import sun from '../assets/logo/sun.svg';
+
+export function EditProfile() {
+    return (
+        <button className="edit-profile">
+            <p>Editer</p>
+        </button>
+    );
+}
